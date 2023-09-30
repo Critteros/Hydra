d0e6f9a02cf0186a9ab6bbfbdb95f3de9d540a28
Szymon Kozioł <contact@critteros.dev>
2023-09-30 22:21:23 +0200

    eslint setup

---
+++ packages/eslint-config-hydra/nest.js
@@ -0,0 +1,8 @@
+/**
+ * Configuration usef for Nest.js apps and packages
+ *
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: ["./node.js"],
+};
---
+++ packages/eslint-config-hydra/next.js
@@ -0,0 +1,8 @@
+/**
+ * Eslint config file used for Next.js apps
+ *
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: ["next", "next/core-web-vitals", "./react.js"],
+};
