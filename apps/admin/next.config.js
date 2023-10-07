56efb7103d5bc7f79952a1eef1e72e6804d7c9f0
Szymon Kozioł <contact@critteros.dev>
2023-10-08 01:40:42 +0200

    Added proxy to the next dev server for API request proxying

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,4 +1,21 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = (phase, { defaultConfig }) => {
+  // For development, proxy requests to the API server
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:8000/api/:path*',
+          },
+        ];
+      },
+    };
+  }
+};
 
 module.exports = nextConfig;
