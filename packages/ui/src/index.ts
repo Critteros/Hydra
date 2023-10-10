8d6d06888eb06d60fbd6b3bd0453f1aa208b27f9
Szymon Kozioł <contact@critteros.dev>
2023-10-10 21:38:15 +0200

    New setup for TS library

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,4 @@
 'use client';
 export * from '@chakra-ui/react';
 export { Link, Image } from '@chakra-ui/next-js';
-export { default as theme, STORAGE_KEY, initialColorMode } from './src/theme';
+export { default as theme, STORAGE_KEY, initialColorMode } from './theme';
