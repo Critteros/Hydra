ee29a39bb1d259d58bbcb940bada850c915ce184
Szymon Kozioł <contact@critteros.dev>
2023-10-11 18:40:38 +0200

    Added empty common package (resolves #7)

---
+++ packages/common/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, type Options } from 'tsup';
+
+export default defineConfig((options: Options) => ({
+  banner: {},
+  splitting: false,
+  entry: ['src/**/*.tsx', 'src/**/*.ts'],
+  format: ['esm'],
+  dts: true,
+  sourcemap: true,
+  minify: false,
+  clean: true,
+  external: ['react', 'react-dom'],
+  ...options,
+}));
