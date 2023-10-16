c86f1bf8f65667de5a7f9c53ae686dbbef74431b
Szymon Kozioł <contact@critteros.dev>
2023-10-16 07:14:47 +0200

    Management module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { loadConfiguration } from './config';
 import { PrismaService } from './prisma.service';
+import { ManagementModule } from './management/management.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PrismaService } from './prisma.service';
       isGlobal: true,
       load: [loadConfiguration],
     }),
+    ManagementModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
