6793f3d1ec3e3ad838830d45d4e77abcbd48615f
Szymon Kozioł <contact@critteros.dev>
2023-10-23 20:39:32 +0200

    CLI: Validation for socket existance

--- apps/cli/src/axios.module.ts
+++ apps/cli/src/axios.module.ts
@@ -1,8 +1,10 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import type { Config } from '@hydra-ipxe/common/server/config';
+import chalk from 'chalk';
+import { access, constants } from 'node:fs/promises';
 
 import { ConfigModule } from './config/config.module';
 
@@ -20,4 +22,23 @@ import { ConfigModule } from './config/config.module';
   ],
   exports: [HttpModule],
 })
-export class AxiosModule {}
+export class AxiosModule implements OnModuleInit {
+  constructor(private readonly configService: ConfigService<Config>) {}
+
+  async onModuleInit() {
+    await this.validateSocketExists();
+  }
+
+  private async validateSocketExists() {
+    const socketPath = this.configService.get('socket.path', { infer: true });
+    if (!socketPath) {
+      console.error(chalk.red('Socket path is not defined.'));
+      process.exit(1);
+    }
+
+    await access(socketPath, constants.F_OK).catch(() => {
+      console.error(chalk.red(`Socket file ${socketPath} does not exist.`));
+      process.exit(1);
+    });
+  }
+}
