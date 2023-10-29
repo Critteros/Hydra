42ffaeb7aea96963a0ad3573fb0b2fa677dabf78
Szymon Kozioł <contact@critteros.dev>
2023-10-30 00:29:06 +0100

    Added helper for redis container creation

--- apps/backend/src/utils/test.ts
+++ apps/backend/src/utils/test.ts
@@ -2,6 +2,7 @@ import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
 
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
+import { GenericContainer, Wait } from 'testcontainers';
 
 import type { PrismaService } from '@/db/prisma.service';
 
@@ -10,13 +11,13 @@ const execAsync = promisify(exec);
 export async function createMockDB(params?: { upsertEnv?: boolean }) {
   const { upsertEnv = true } = params ?? {};
 
-  const postgresqlContainer = await new PostgreSqlContainer().start();
+  const postgresqlContainer = await new PostgreSqlContainer('postgres:16').start();
   const DATABASE_URL = postgresqlContainer.getConnectionUri();
   const env = {
     ...process.env,
     DATABASE_URL,
   };
-  await execAsync('npx prisma db push --skip-generate', { env: env });
+  await execAsync('npx prisma db push --skip-generate', { env });
   if (upsertEnv) {
     process.env = env;
   }
@@ -24,6 +25,28 @@ export async function createMockDB(params?: { upsertEnv?: boolean }) {
   return {
     container: postgresqlContainer,
     DATABASE_URL,
+  };
+}
+
+export async function createMockRedis(params?: { upsertEnv?: boolean }) {
+  const { upsertEnv = true } = params ?? {};
+
+  const redisContainer = await new GenericContainer('redis:7')
+    .withExposedPorts(6379)
+    .withWaitStrategy(Wait.forLogMessage('Ready to accept connections'))
+    .start();
+  const REDIS_URL = `redis://${redisContainer.getHost()}:${redisContainer.getMappedPort(6379)}`;
+  const env = {
+    ...process.env,
+    REDIS_URL,
+  };
+  if (upsertEnv) {
+    process.env = env;
+  }
+
+  return {
+    container: redisContainer,
+    REDIS_URL,
   };
 }
 
