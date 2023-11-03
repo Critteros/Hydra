7a63bdb7dfb72f286125b377dabc82f23d9d01d3
Szymon Kozioł <contact@critteros.dev>
2023-11-03 14:01:38 +0100

    Fix for shadcui dialog animation preventing user clicks

--- apps/web/src/app/dashboard/users/users-table-actions.tsx
+++ apps/web/src/app/dashboard/users/users-table-actions.tsx
@@ -1,10 +1,21 @@
 'use client';
 
+import { useState, useRef, type ReactNode } from 'react';
+
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import type { Row } from '@tanstack/react-table';
-import { PencilLine, ClipboardCopy } from 'lucide-react';
+import { PencilLine, ClipboardCopy, KeyRound } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -20,12 +31,47 @@ import type { User } from './queries';
 type UsersTableActionsProps = {
   row: Row<User>;
 };
-
+type DialogItemProps = {
+  trigger: ReactNode;
+  onSelect: () => void;
+  onOpenChange: (open: boolean) => void;
+  children: ReactNode;
+};
+function DialogItem({ trigger, onSelect, onOpenChange, children }: DialogItemProps) {
+  return (
+    <Dialog onOpenChange={onOpenChange}>
+      <DialogTrigger asChild>
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault();
+            onSelect?.();
+          }}
+        >
+          {trigger}
+        </DropdownMenuItem>
+      </DialogTrigger>
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
+  );
+}
 export function UsersTableActions({ row }: UsersTableActionsProps) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [hasOpenDialog, setHasOpenDialog] = useState(false);
+  const dropdownTriggerRef = useRef<HTMLButtonElement>(null);
+  const focusRef = useRef<HTMLElement | null>(null);
   const { toast } = useToast();
 
-  const user = row.original;
+  const handleDialogItemSelect = () => {
+    focusRef.current = dropdownTriggerRef.current;
+  };
+  const handleDialogItemOpenChange = (open: boolean) => {
+    setHasOpenDialog(open);
+    if (open === false) {
+      setDropdownOpen(false);
+    }
+  };
 
+  const user = row.original;
   const onCopyToClipboard = (field: keyof User) => async () => {
     const value = user[field];
     if (!value) return;
@@ -35,25 +81,51 @@ export function UsersTableActions({ row }: UsersTableActionsProps) {
       description: `The ${field} has been copied to the clipboard`,
     });
   };
+
   return (
-    <DropdownMenu>
+    <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+        <Button
+          variant="ghost"
+          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          ref={dropdownTriggerRef}
+        >
           <DotsHorizontalIcon className="h-4 w-4" />
           <span className="sr-only">Open menu</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent
+        hidden={hasOpenDialog}
+        onCloseAutoFocus={(event) => {
+          event.preventDefault();
+          if (focusRef.current) {
+            focusRef.current?.focus();
+            focusRef.current = null;
+          }
+        }}
+      >
         <DropdownMenuLabel>Actions</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <PencilLine className="mr-2 h-4 w-4" />
-          <span>Edit</span>
-        </DropdownMenuItem>
         <DropdownMenuItem onClick={onCopyToClipboard('uid')}>
           <ClipboardCopy className="mr-2 h-4 w-4" />
           <span>Copy UID</span>
         </DropdownMenuItem>
+        <DropdownMenuItem>
+          <PencilLine className="mr-2 h-4 w-4" />
+          <span>Edit</span>
+        </DropdownMenuItem>
+        <DialogItem
+          trigger={
+            <>
+              <KeyRound className="mr-2 h-4 w-4" />
+              <span>Change password</span>
+            </>
+          }
+          onSelect={handleDialogItemSelect}
+          onOpenChange={handleDialogItemOpenChange}
+        >
+          ttt
+        </DialogItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- apps/web/src/components/ui/dialog.tsx
+++ apps/web/src/components/ui/dialog.tsx
@@ -39,7 +39,17 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full',
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6',
+        'shadow-lg duration-200',
+        'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full',
+        'data-[state=open]:animate-in',
+        // FIXME: The following classname is not owkring and causes problems when closing dialog
+        //      After the dialog is closed it seems like the html element is on top of everything
+        //      and the user can't click anywhere
+        //      for now 'animate-out' is used instead
+        // 'data-[state=closed]:animate-out',
+        'animate-out',
+        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
         className,
       )}
       {...props}
