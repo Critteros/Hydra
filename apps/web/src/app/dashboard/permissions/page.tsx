e8e5f6a454c80defd4be09817299de62ad42f625
Szymon Kozioł <contact@critteros.dev>
2023-11-04 15:15:38 +0100

    Table with permissions

--- apps/web/src/app/dashboard/permissions/page.tsx
+++ apps/web/src/app/dashboard/permissions/page.tsx
@@ -1,5 +1,15 @@
 import 'server-only';
 
+import { ScrollArea } from '@/components/ui/scroll-area';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { Typography } from '@/components/ui/typography';
 import { getClient } from '@/lib/server/apollo-client';
 
@@ -9,6 +19,31 @@ export default async function DashboardPermissionsPage() {
   const {
     data: { permissions },
   } = await getClient().query({ query: queryAllPermissions });
-  console.log(permissions);
-  return <Typography variant="h1">Permissions</Typography>;
+
+  return (
+    <ScrollArea className="flex min-h-0 grow items-center justify-center">
+      <main className="flex grow flex-col items-center justify-center gap-10 px-4">
+        <Typography variant="h1" className="mb-4 self-start">
+          Permissions
+        </Typography>
+        <Table>
+          <TableCaption>Available permissions</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Permission</TableHead>
+              <TableHead>Description</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {permissions.map(({ id, description }, index) => (
+              <TableRow key={`${id}-${index}`}>
+                <TableCell className="font-semibold">{id}</TableCell>
+                <TableCell>{description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </ScrollArea>
+  );
 }
