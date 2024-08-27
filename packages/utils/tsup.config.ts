c9410d759a01ed028b1c2ffb900d428094eeee36
Szymon Kozioł <szymonkoziol3@gmail.com>
2024-08-28 00:13:11 +0200

    [utils] Updated package exports

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -3,7 +3,14 @@ import { defineConfig, type Options } from 'tsup';
 export default defineConfig((options: Options) => ({
   banner: {},
   splitting: true,
-  entry: ['src/index.ts', 'src/types/index.ts', 'src/object.ts', 'src/compare.ts', 'src/errors.ts'],
+  entry: [
+    'src/index.ts',
+    'src/types/index.ts',
+    'src/object.ts',
+    'src/compare.ts',
+    'src/errors.ts',
+    'src/async.ts',
+  ],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
